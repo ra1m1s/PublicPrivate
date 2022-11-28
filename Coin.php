c05ddf6d7fe037bfb2e9869907905fab7037ccef
ra1m1s <raimund.rinkevic@gmail.com>
2022-11-28 21:00:20 +0200

    first commit

---
+++ Coin.php
@@ -0,0 +1,21 @@
+<?php
+
+class coin {
+    private $salis;
+    private $verte;
+    private $metai;
+    private $medziaga;
+
+    function __construct ($salis = null, $verte = null, $metai = null, $medziaga = null){
+       
+    }
+}
+
+
+
+
+
+
+
+
+?>
